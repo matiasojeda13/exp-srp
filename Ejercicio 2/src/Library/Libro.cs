90f9522445b23592b0082e19ac21b0a676c3f21b
matiasojeda13 <matias.ojeda@correo.ucu.edu.com>
2020-09-01 23:24:06 -0300

    comentarios del ejercicio 2

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -4,7 +4,10 @@ namespace SRP
 {
     public class Libro
     {
-
+        /*no se cumple con srp debido a que se le asigna la responsabilidad de almacenarse a la clase libro, la cual no deberia 
+        preocuparse de ello, y si deberia hacerlo una clase aparte llamada biblioteca
+        ademas no se cumple con el patron expert dado que se le asignan los datos de almacenaje a una clase que no debiera ser
+        experta en ello */
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
